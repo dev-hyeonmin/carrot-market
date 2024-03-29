2ce957edb0ab68d05942b7f19266ffe1d20801a0
dev-hyeonmin <dev.hyeonmin@gmail.com>
2024-03-29 16:11:22 +0900

    Login, SMS Page

---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+import Button from "@/components/actions/button";
+import Input from "@/components/form/input";
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+
+      <form className="flex flex-col gap-3">
+        <Input
+          type="text"
+          placeholder="Email"
+          required />
+
+        <Input
+          type="text"
+          placeholder="Password"
+          required />
+
+        <Button 
+          text="Create Account"/>
+      </form>
+
+      <div className="w-full h-px bg-neutral-500" />
+
+      <div>
+        <Link href="/sms" className="primary-btn flex items-center justify-center h-10 gap-3">
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+          </span>
+          <span>Sign up with SMS</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ app/sms/page.tsx
@@ -0,0 +1,28 @@
+import Button from "@/components/actions/button";
+import Input from "@/components/form/input";
+
+export default function SMS() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col *:font-medium">
+        <h1 className="text-2xl">SMS Login</h1>
+        <h2 className="text-xl">Verify your phone number.</h2>
+      </div>
+
+      <form className="flex flex-col gap-3">
+        <Input
+          type="text"
+          placeholder="Phone number"
+          required />
+
+        <Input
+          type="text"
+          placeholder="Verification code"
+          required />
+
+        <Button 
+          text="Verify"/>
+      </form>
+    </div>
+  )
+}
